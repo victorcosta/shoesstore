013794b998cc9fef60aecf718d99e4d522e96314
Victor Costa <victorcostadev@gmail.com>
2019-09-17 03:50:15 -0300

    increment and decrement product on cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,9 +4,7 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(
-          product => product.id === action.product.id
-        );
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
 
         if (productIndex >= 0) {
           draft[productIndex].amount += 1;
@@ -19,14 +17,29 @@ export default function cart(state = [], action) {
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(
-          product => product.id === action.id
-        );
+        const productIndex = draft.findIndex(p => p.id === action.id);
 
         if (productIndex >= 0) {
           draft.splice(productIndex, 1);
         }
       });
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return produce(state, draft => {
+          const productIndex = draft.findIndex(p => p.id === action.id);
+          if (productIndex >= 0) {
+            draft.splice(productIndex, 1);
+          }
+        });
+      }
+
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }
