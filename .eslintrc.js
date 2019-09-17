57a2f49d6e80e11da29a84e08e26e540e142abf6
Victor Costa <victorcostadev@gmail.com>
2019-09-17 03:02:22 -0300

    adding immer and removing cart product

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
+    'no-param-reassign': 'off',
     'no-console': ['error', { allow: ['tron'] }],
     // 'react/no-unused-state': 'off',
   },
